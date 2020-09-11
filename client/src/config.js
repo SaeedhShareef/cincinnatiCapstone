c2924dceb31cb82785cdb5e055229e52df798e4d
Saeed Shareef <saeedhshareef@gmail.com>
2020-09-11 18:05:09 -0400

    config update

--- client/src/config.js
+++ client/src/config.js
@@ -4,6 +4,6 @@ dotenv.config();
 export const BACKEND_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:5000/'
-    : 'https://explorecincinnati.herokuapp.com/';
+    : 'https://cincinnaticapstone.herokuapp.com/';
 // comment out this file and procfile and your imports for the config file if it doesn't build after this
 //this one works ignore top comment
