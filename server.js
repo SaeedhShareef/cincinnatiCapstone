ddcd591ab5e1a39dd0e1935026e344e93cec18fc
DJ Stewart <deshastewart1992@gmail.com>
2020-09-10 23:13:12 -0700

    fix

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 // const requirejs = require('requirejs');
 const express = require('express');
 const cors = require('cors');
+var bodyParser = require('body-parser')
+
 // const mongoose = require('mongoose');
 const mongoose = require('mongoose')
+const TouristDestination = require("./models/addLocations.models");
+const Contact = require("./models/contacts.models");
  require('dotenv').config();
+
+ var jsonParser = bodyParser.json
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,13 +39,23 @@ connection.once('open', () => {
 // mongoose.connection.on('open', () => {  console.log("MongoDB database connection established successfully");})
 // mongoose.connection.on('error', (err) => {  console.log(err);})
 
-const addLocationsRouter = require('./models/addLocations.models');
-const contactsRouter = require('./models/contacts.models');
+// const addLocationsRouter = require('./models/addLocations.models');
+// const contactsRouter = require('./models/contacts.models');
 
   
 
-app.use('/addLocations', addLocationsRouter);
-app.use('/contacts', contactsRouter);
+app.use('/all-locations', TouristDestination);
+app.use('/contacts', Contact);
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+
+
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/client/build/index.html', function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
+  });
+  
